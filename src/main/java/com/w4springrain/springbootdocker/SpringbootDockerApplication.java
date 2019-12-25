0d5434bc2262581590cc0c3c6e0248748ae3fc90
moregorenine <moregorenine@gmail.com>
2019-12-25 13:58:29 +0900

    Init Project

---
+++ src/main/java/com/w4springrain/springbootdocker/SpringbootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.w4springrain.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/w4springrain/springbootdocker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w4springrain.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
